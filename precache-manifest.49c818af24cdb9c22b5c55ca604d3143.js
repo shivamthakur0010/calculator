4a74c255e42fb5ee9a3ab026a08cb2717ba4b85d
shivamthakur0010 <demonking0010@gmail.com>
2023-04-25 01:13:25 +0530

    Updates

---
+++ precache-manifest.49c818af24cdb9c22b5c55ca604d3143.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0446a408a1d7da1065939a4e9ad8adf",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "d0a3ab3376be2d04f64a",
+    "url": "/calculator/static/css/main.6fdcdd19.chunk.css"
+  },
+  {
+    "revision": "b011ce21a6da794d9886",
+    "url": "/calculator/static/js/2.add7bac3.chunk.js"
+  },
+  {
+    "revision": "d0a3ab3376be2d04f64a",
+    "url": "/calculator/static/js/main.2d9ba8bb.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
